bb1bacd8f93b059fb7fe634e20b4028f7acfbbf9
Adrian Veppo <veppoadrian98@gmail.com>
2025-04-29 16:29:34 -0300

    ajuste letras

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home(){
     return(<div className="home">
         <div className="home_image_container">
             <p className='home_paragraph'>Psicóloga Especialista en<br></br>Psicoterapia Cognitiva Integrativa<br></br>
-            <label style={{fontSize: "27px"}}>M.P. 667</label></p>
+            <label style={{fontSize: "20px"}}>M.P. 667</label></p>
             
             <img className='home_image' src={HomeImage}></img>
         </div>

d71944f6d3a47ad086f082bb476d654373572bdd
Adrian Veppo <veppoadrian98@gmail.com>
2025-05-02 15:59:04 -0300

    ajuste opacidad

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,34 @@
 import './home.css';
 import HomeImage from '../assets/Brain_NoBackground.png'
+import { useEffect, useRef } from 'react';
 
 
 function Home(){
+
+    const myRef = useRef(null)
+
+    useEffect(()=>{
+        const observer = new IntersectionObserver((entries, observer)=>{
+            entries.forEach((entry)=>{
+                if(entry.isIntersecting){
+                    entry.target.classList.add("visible")
+                    observer.unobserve(entry.target)
+                }
+            })
+        })
+        
+        if(myRef.current){
+            observer.observe(myRef.current)
+        }
+
+        
+    }, [])
+
     
-    return(<div className="home">
-        <div className="home_image_container">
+
+
+    return(<div className="home" >
+        <div className="home_image_container" ref={myRef}>
             <p className='home_paragraph'>Psicóloga Especialista en<br></br>Psicoterapia Cognitiva Integrativa<br></br>
             <label style={{fontSize: "20px"}}>M.P. 667</label></p>
             
